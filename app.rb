2db59912ca7d5a8998f5940a4bccbe5a70f0d8f2
Ivo ánchez Checa Crosato <ivoscc@gmail.com>
2013-11-08 20:08:24 -0500

    merge and such

--- app.rb
+++ app.rb
@@ -7,43 +7,19 @@ Cuba.plugin Cuba::Render
 Cuba.use Rack::Session::Cookie,
   secret: "__a_very_long_string__"
 
-Cuba.use Rack::Static,
-  root: "public",
-  urls: ["/js", "/css", "/less", "/img"]
-
-Cuba.plugin Cuba::Render
-$data = [
-    "Noticia 1",
-    "Noticia 2",
-    "Noticia 3",
-    "Noticia 4",
-    "Noticia 5",
-]
-$JSON_NAME = "data.json"
-
 def read_json_news( filename )
     begin
         JSON.parse(IO.read(filename))
     rescue => _
-        [{}]
+        []
     end
-end
-
-def get_random_new()
-    r = Random.new
-    json_data = read_json_news($JSON_NAME)
-    r_number = r.rand(0..json_data.length-1)
-    json_data[r_number]["data"]
 end
 
 Cuba.define do
   on get do
     on root do
-      res.write render("home.erb", content: "hello, world")
-    end
-
-    on "get_data" do
-        res.write get_random_new()
+      muhlist = read_json_news("data.json")
+      res.write render("home.erb", content: "hello, world", muhlist: muhlist)
     end
 
     on "css", extension("css") do |file|
