9d62932ef2d576e177c34f7293249c58b92e8bcf
Giorgio Leveroni <giorgio.m.leveroni@gmail.com>
2013-11-08 19:13:33 -0500

    Basic template

--- app.rb
+++ app.rb
@@ -1,12 +1,17 @@
 require "cuba"
 
+require "cuba/render"
+
+
 Cuba.use Rack::Session::Cookie,
   secret: "__a_very_long_string__"
+
+Cuba.plugin Cuba::Render
 
 Cuba.define do
   on get do
     on root do
-      res.write "ollanta in the house"
+      res.write render("home.erb", content: "hello, world")
     end
   end
 end
