511d450903d14bbd5a5226250469956bc1aa9d87
Ivo ánchez Checa Crosato <ivoscc@gmail.com>
2013-11-08 19:02:06 -0500

    get random stuff

--- app.rb
+++ app.rb
@@ -3,10 +3,27 @@ require "cuba"
 Cuba.use Rack::Session::Cookie,
   secret: "__a_very_long_string__"
 
+$data = [
+    "Noticia 1",
+    "Noticia 2",
+    "Noticia 3",
+    "Noticia 4",
+    "Noticia 5",
+]
+
+def get_random_new()
+    r = Random.new
+    $data[r.rand(0..$data.length-1)]
+end
+
 Cuba.define do
   on get do
     on root do
       res.write "ollanta in the house"
+    end
+
+    on "get_data" do
+        res.write get_random_new()
     end
   end
 end
